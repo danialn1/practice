d7b08fe1880e9fa256653a5548fd770efbf32a3d
danialn1 <97178122+danialn1@users.noreply.github.com>
2023-02-04 16:54:19 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -3,14 +3,20 @@ class Wallet:
   def __init__(self, balance):
     self.balance = 0
     
-  def setBalance(int bal):
-    balance=self.bal
+  def setBalance(self,val):
+    self.balance=val
+    
+  def getBalance(self):
+    return self.balance
   
     
-  def removeBalance():
-    balance=self.bal-balance
+  def removeBalance(self,val):
+    self.balance-= val
     
-    
+
+
+
+
   
     
   
